30d73b21ba5cb02635634f93fae3d176b6533ace
Ricardo Porfírio <89818885+Ricardoporfiriovieira@users.noreply.github.com>
2022-07-09 11:16:31 -0300

    Add files via upload

---
+++ exercicio logica de programação - formação acelerada/atividade1/main.js
@@ -0,0 +1,33 @@
+let a = 5;
+let b =  3;
+
+function soma(x,y){
+    return x + y;
+}
+
+function mult(x, y){
+    return x * y;
+}
+
+function div(x, y){
+    return x / y;
+}
+
+function sub(x, y){
+    return x - y;
+}
+
+console.log(soma(a,b));
+console.log(mult(a,b));
+console.log(div(a,b));
+console.log(sub(a,b));
+
+
+
+
+
+
+
+
+
+
